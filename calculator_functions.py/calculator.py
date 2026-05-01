a2208646c8e54ee64899eccf6c4ac9037cad23fa
xyriltumbali-Pupians <xyriltumbali@gmail.com>
2026-05-01 22:41:11 +0800

    finished_calculator_code

--- calculator_functions.py/calculator.py
+++ calculator_functions.py/calculator.py
@@ -15,7 +15,7 @@ class CalculatorApp:
         print("[4] Divide")
         print("[5] View History")
         print("[0] Exit")
-        
+
     def get_numbers(self):
         try:
             a = float(input("Enter first number: "))
@@ -31,3 +31,44 @@ class CalculatorApp:
             "3": Multiplication(),
             "4": Division()
         }
+
+        operation = operations.get(choice)
+        if not operation:
+            raise InvalidInputError("Invalid operation selected!")
+
+        return operation.calculate(a, b)
+
+    def save_history(self, a, b, choice, result):
+        symbols = {"1": "+", "2": "-", "3": "*", "4": "/"}
+        self.history.append(f"{a} {symbols.get(choice)} {b} = {result}")
+
+    def show_history(self):
+        if not self.history:
+            print("No history yet.")
+        else:
+            for i, record in enumerate(self.history, 1):
+                print(f"{i}. {record}")
+
+    def run(self):
+        while True:
+            self.display_menu()
+            choice = input("Choose an option: ")
+
+            if choice == "0":
+                print("Thank you!\U0001F431 MEOW")
+                break
+
+            if choice == "5":
+                self.show_history()
+                continue
+
+            try:
+                a, b = self.get_numbers()
+                result = self.perform_operation(choice, a, b)
+
+                print(f"Result: {result}")
+                self.save_history(a, b, choice, result)
+                self.last_result = result
+
+            except CalculatorError as e:
+                print(f"Error: {e}")
